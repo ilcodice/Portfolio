2785a4413c2e066d5a1be58609849fa18b1da0b2
Delshad <elcodius@gmail.com>
2024-01-10 12:54:09 +0100

    modified

--- assets/js/form.js
+++ assets/js/form.js
@@ -11,13 +11,10 @@ function validation(){
         alert("Email should end with @dci-student.org")
         return false
     }
- 
     if (firstname.length < 8 ) {
         alert("first name should be more than 8")
         return false
-
     }
-    
     if (lastname.length == 0){
         alert("last name should be provided")
         return false
@@ -30,4 +27,18 @@ function validation(){
     }
     
 
+}
+function activateSubmit(){
+    const email = document.registration.email.value
+    const firstname = document.registration.first_name.value
+    const lastname = document.registration.last_name.value
+    const submitBtn = document.getElementById("submit")
+
+    if (email.length > 0 && firstname.length > 0 && lastname.length > 0 ){
+        // remove submit-disabled"class from submit button"
+        submitBtn.classList.remove("submit-disabled")
+    } else { 
+        //add submit-disabled class if it doesent exist
+        submitBtn.classList.add("submit-disabled")
+    }
 }
