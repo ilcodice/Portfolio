ed679b842e345ca9465c2686e38054d3f5577e3b
Delshad <elcodius@gmail.com>
2024-01-10 09:28:18 +0100

    from js validation added

---
+++ assets/js/form.js
@@ -0,0 +1,33 @@
+
+function validation(){
+
+    const email = document.registration.email.value
+    const firstname = document.registration.first_name.value
+    const lastname = document.registration.last_name.value
+    const gender = document.registration.gender.value
+    const country = document.registration.country.value 
+
+    if (email.endsWith("@dci-student.org") == false){ 
+        alert("Email should end with @dci-student.org")
+        return false
+    }
+ 
+    if (firstname.length < 8 ) {
+        alert("first name should be more than 8")
+        return false
+
+    }
+    
+    if (lastname.length == 0){
+        alert("last name should be provided")
+        return false
+    }
+        
+
+    if (country == "usa" && gender != "female"){
+         alert("we only register females in usa")
+         return false
+    }
+    
+
+}
